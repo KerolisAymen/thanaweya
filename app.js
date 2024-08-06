8934162cdcb92a8ea1ea6d91317138a8ee4baa9f
KerolisAymen <kerolisaymen456@gmail.com>
2024-08-06 21:09:27 +0300

    search by name

--- app.js
+++ app.js
@@ -28,17 +28,59 @@ app.get("/" , (req,res)=>{
 })
 app.post("/getdata", async(req, res) => {
   console.log(req.body.seating) ; 
-    const seating = req.body.seating;
-    const student  = await Student.findOne({seating : seating}) ; 
+    const search = req.body.seating;
+    if (isNaN(search)){
+      const name  = search ; 
+      const re = new RegExp(name); 
+      const students  = await Student.find({name : { $regex: re ,$options:'i' }}) ; 
+      
+      if(students != undefined){
+      console.log(students); 
+      res.send(JSON.stringify(students)) ; 
+    }
+    }
+    else
+    {
+      const seating = search ; 
+    const student  = await Student.find({seating : seating}) ; 
     if(student!=undefined) {
       console.log(student);
       console.log(JSON.stringify(student));
         res.send(JSON.stringify(student)) ; 
     }
+  }
 });
+
+
+
+// const formData = new URLSearchParams();
+// formData.append('seating_no', '840000');
+
+// fetch("https://natega.youm7.com/Home/result" , {
+
+//   method: "POST", 
+//   headers : {
+//     "Content-Type" : "application/x-www-form-urlencoded" , 
+//     "Origin" : "https://natega.youm7.com" ,
+//     "Refer" : "https://natega.youm7.com"
+//   },
+//   body : formData
+
+
+// }).then(response => response.text()) // Parse the response as text
+// .then(html => {
+//   console.log('Response:', html);
+  
+// })
+// .catch(error => {
+//   console.error('Error:', error.message);
+// });
+
 
 
 const port = process.env.PORT || 3000;
 app.listen(port , ()=>{
   console.log("listening to port " + port) ; 
 })
+
+
